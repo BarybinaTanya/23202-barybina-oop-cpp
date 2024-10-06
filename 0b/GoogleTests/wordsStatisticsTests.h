b543f97b06fcce77081733e3d9ec0dc628a566e9
Татьяна Барыбина <159711175+BarybinaTanya@users.noreply.github.com>
2024-10-06 21:03:13 +0700

    Create wordsStatisticsTests.h

---
+++ 0b/GoogleTests/wordsStatisticsTests.h
@@ -0,0 +1,63 @@
+#ifndef INC_0B_WORDSSTATISTICSTESTS_H
+#define INC_0B_WORDSSTATISTICSTESTS_H
+
+#include <gtest/gtest.h>
+#include <fstream>
+#include "../Splitter.h"
+#include "../Reader.h"
+#include "../WordsStatistics.h"
+
+void createTestFileType4 (std::string newFileName) {
+    std::ofstream test_file(newFileName);
+    test_file << "Everything is fine! Everything is fine!\nEverything is fine! Everything is fine!";
+    test_file.close();
+}
+
+TEST(WordsStatisticsTests, GetNumberWordsContainedTest) {
+    WordsStatistics wordsStatisticsGetter;
+    EXPECT_EQ(wordsStatisticsGetter.getNumberWordsContained(), 0);
+}
+
+TEST(WordsStatisticsTests, GetAllKeyWordsTest) {
+    createTestFileType4(splitterTestFiles[3]);
+    Reader readWizard;
+    readWizard.openFile(splitterTestFiles[3]);
+    readWizard.readAllLines();
+
+    Splitter splitWizard;
+    splitWizard.splitText(readWizard.allLines, true);
+
+    WordsStatistics wordsStatisticsGetter;
+    wordsStatisticsGetter.getAllKeyWords(splitWizard.splittedText);
+
+    EXPECT_EQ(wordsStatisticsGetter.allKeyWordsMet.size(), 3);
+    printSplittedText(wordsStatisticsGetter.allKeyWordsMet);
+}
+
+void printMap(std::map<string, std::pair<int, float>> wordsFrequencyStatistics) {
+    for (const auto &pair : wordsFrequencyStatistics) {
+        std::cout << pair.first << ": ("
+                  << pair.second.first << ", "
+                  << pair.second.second << ")"
+                  << std::endl;
+    }
+}
+
+TEST(WordsStatisticsTests, GetFullWordsStatisticsTest) {
+    Reader readWizard;
+    readWizard.openFile(splitterTestFiles[3]);
+    readWizard.readAllLines();
+
+    Splitter splitWizard;
+    splitWizard.splitText(readWizard.allLines, true);
+
+    WordsStatistics wordsStatisticsGetter;
+    wordsStatisticsGetter.getFullFrequencyMap(splitWizard.splittedText);
+
+    EXPECT_EQ(wordsStatisticsGetter.wordsFrequencyStatistics[wordsStatisticsGetter.allKeyWordsMet[0]].first, 4);
+    EXPECT_EQ(wordsStatisticsGetter.wordsFrequencyStatistics[wordsStatisticsGetter.allKeyWordsMet[1]].first, 4);
+    EXPECT_EQ(wordsStatisticsGetter.wordsFrequencyStatistics[wordsStatisticsGetter.allKeyWordsMet[2]].first, 4);
+    printMap(wordsStatisticsGetter.wordsFrequencyStatistics);
+}
+
+#endif //INC_0B_WORDSSTATISTICSTESTS_H
